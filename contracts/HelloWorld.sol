3563e2f16f9954637a965916db5facbd4aec2d4e
saurabh-solkar16 <30970517+saurabh-solkar16@users.noreply.github.com>
2022-03-06 15:37:28 +0530

    first commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract HelloWorld{
+    function getMessage() public pure returns(string memory){
+        return "God Is Great 1";
+    }
+}
